c55771ec25c502d709aa336a9a5a85900e140d53
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-17 14:07:21 +0900

    Requirements.txt and Repo links that have already trained the models being used

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 # Colourizing Black & White Images using Python
+import numpy as np 
+import cv2
+
+# Using Github Repo to train models
+# Models: https://github.com/richzhang/colorization/tree/caffe/colorization/models
+# Points: https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
