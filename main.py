ecec586e02e110f2b56abdefaa6655bdf3227ca5
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-17 14:29:03 +0900

    Defining and loading the layers

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ model_path = 'models/dummy.caffemodel'
 kernel_path = 'models/pts_in_hull.npy'
 image_path = 'image_one.jpg'
 
+neural_network = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+points = np.load(kernel_path)
+
+points = points.transpose().reshape(2, 313, 1, 1)
+# LAB (lightness, A and B are colour values)
+neural_network.getLayer(neural_network.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]
+neural_network.getLayer(neural_network.getLayerId("conv8_313_rn")).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
