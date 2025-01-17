7c708bcebb33dda5cc19dce0ef0fb434fb48caf9
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-17 14:11:45 +0900

    Models Directory Created

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ import cv2
 # Using Github Repo to train models
 # Models: https://github.com/richzhang/colorization/tree/caffe/colorization/models
 # Points: https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+
